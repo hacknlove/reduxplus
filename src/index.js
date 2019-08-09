a54d3ce9a84ce11d683ee2d74bac916fc977f6e5
antonio <antonio@pykiss.com>
2019-08-09 15:37:29 +0200

    subscribeKey

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ const useRedux = require('./useRedux')
 const hydrate = require('./hydrate')
 const store = require('./store')
 const subStore = require('./subStore')
+const subscribeKey = require('./subscribeKey')
 
 store.setReducer = setReducer
 store.setMiddleware = setMiddleware
 store.hydrate = hydrate
 store.useRedux = useRedux
 store.subStore = subStore
+store.subscribeKey = subscribeKey
 
 module.exports = store
