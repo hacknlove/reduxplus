8897cf0496a76b3bd7a667cbf743568bb3e0c0d0
antonio <antonio@pykiss.com>
2019-08-10 19:17:16 +0200

    subs improved

--- src/subStore.js
+++ src/subStore.js
@@ -1,29 +1,53 @@
 const store = require('./store')
 const getValue = require('./getValue')
 const setValue = require('./setValue')
-const isDifferent = require('isdifferent')
+
+store.subs = {}
+
+store.setReducer((state, action) => {
+  if (action.type !== '__reduxplus_subStore') {
+    return state
+  }
+  if (!subReducers[action.__reduxplus_subStore_key]) {
+    return state
+  }
+
+  return setValue(
+    state,
+    action.__reduxplus_subStore_key,
+    subReducers[action.__reduxplus_subStore_key].reduce(
+      (state, reducer) => reducer(state, action.action),
+      getValue(state, action.__reduxplus_subStore_key)
+    )
+  )
+})
+
+const subReducers = {}
+const subSubscriptions = {}
+
 class SubStore {
   constructor (key) {
     this.key = key
   }
 
   getState () {
+    if (store.subs[this.key]) {
+      throw new Error('sub forgotten')
+    }
     return getValue(store.getState(), this.key)
   }
 
   setReducer (reducer) {
-    store.setReducer((state, action) => {
-      if (action.type !== '__reduxplus_subStore') {
-        return state
-      }
-      if (action.__reduxplus_subStore_key !== this.key) {
-        return state
-      }
-      return setValue(state, this.key, reducer(getValue(state, this.key), action.action))
-    })
+    if (store.subs[this.key]) {
+      throw new Error('sub forgotten')
+    }
+    subReducers[this.key].push(reducer)
   }
 
   dispatch (action) {
+    if (store.subs[this.key]) {
+      throw new Error('sub forgotten')
+    }
     store.dispatch({
       type: '__reduxplus_subStore',
       __reduxplus_subStore_key: this.key,
@@ -32,25 +56,34 @@ class SubStore {
   }
 
   useRedux (key) {
+    if (store.subs[this.key]) {
+      throw new Error('sub forgotten')
+    }
     return store.useRedux(`${this.key}.${key}`)
   }
 
-  subscribe (listener) {
-    var current = this.getState()
-    return store.subscribe(() => {
-      var newValue = this.getState()
-      if (isDifferent(current, newValue)) {
-        current = newValue
-        listener()
-      }
-    })
+  subscribe (callback) {
+    if (store.subs[this.key]) {
+      throw new Error('sub forgotten')
+    }
+    const unsuscribe = store.subscribeKey(`${this.key}`, callback)
+    subSubscriptions[this.key].push(unsuscribe)
+    return unsuscribe
   }
 
   subscribeKey (key, callback) {
-    return store.subscribeKey(`${this.key}.${key}`, callback)
+    if (store.subs[this.key]) {
+      throw new Error('sub forgotten')
+    }
+    const unsuscribe = store.subscribeKey(`${this.key}.${key}`, callback)
+    subSubscriptions[this.key].push(unsuscribe)
+    return unsuscribe
   }
 
   hydrate (newState, replace = false) {
+    if (store.subs[this.key]) {
+      throw new Error('sub forgotten')
+    }
     if (!replace) {
       newState = {
         ...this.getState(),
@@ -61,12 +94,33 @@ class SubStore {
   }
 
   subStore (key) {
+    if (store.subs[this.key]) {
+      throw new Error('sub forgotten')
+    }
     return new SubStore(`${key}.${this.key}`)
+  }
+
+  forgot () {
+    if (store.subs[this.key]) {
+      throw new Error('sub forgotten')
+    }
+    delete store.subs[this.key]
+    delete subReducers[this.key]
+    subSubscriptions.forEach(f => f())
+    delete subSubscriptions[this.key]
   }
 }
 
 function subStore (key) {
-  return new SubStore(key)
+  if (store.subs[key]) {
+    return store.subs[key]
+  }
+
+  store.subs[key] = new SubStore(key)
+  subReducers[key] = []
+  subSubscriptions[key] = []
+
+  return store.subs[key]
 }
 
 module.exports = subStore
